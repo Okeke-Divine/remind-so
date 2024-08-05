4afb8f4fcaa6b3b4523c663b35f8170520ff2b6b
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-08-06 00:58:52 +0100

    Updated the wait list page

--- src/components/waitlist/WaitListPage.jsx
+++ src/components/waitlist/WaitListPage.jsx
@@ -6,28 +6,29 @@ export default function WaitListPage() {
         <div className="relative w-full min-h-[100vh]">
             {/* Background Image */}
             <div
-                className="absolute inset-0 bg-cover bg-center bg-[url('/images/bg.png')]"
+                className="fixed inset-0 bg-cover bg-center"
+                style={{ backgroundImage: "url('/images/bg.png')" }}
             ></div>
 
             {/* Black Overlay with Glass Effect */}
-            <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-md flex justify-center items-center">
-                <div className="relative z-10 px-5 md:px-10 py-10 lg:px-16 flex flex-col items-center text-white">
-                    {/* app name */}
-                    <div className="mb-5">
-                        <h2 className="uppercase text-center tracking-widest">
-                            {config.appName}.so
-                        </h2>
-                    </div>
+            <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-md"></div>
 
-                    <h1 className="text-center">
-                        To-dos and Schedules in one place.
-                    </h1>
+            <div className="relative z-10 px-5 md:px-10 py-16 lg:px-16 flex flex-col items-center text-white min-h-[100vh]">
+                {/* app name */}
+                <div className="mb-5">
+                    <h2 className="uppercase text-center tracking-widest">
+                        {config.appName}.so
+                    </h2>
+                </div>
 
-                    {/* form */}
-                    <div className="mt-5">
-                        <h3 className="text-center">Never miss a task again!</h3>
-                        <WaitListForm />
-                    </div>
+                <h1 className="text-center">
+                    To-dos and Schedules in one place.
+                </h1>
+
+                {/* form */}
+                <div className="mt-5">
+                    <h3 className="text-center">Never miss a task again!</h3>
+                    <WaitListForm />
                 </div>
             </div>
         </div>
