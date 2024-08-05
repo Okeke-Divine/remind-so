efe10f7a5dd6e32525148f20897292021ae3dd65
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-08-06 00:53:22 +0100

    Updated the waitlist background

--- src/components/waitlist/WaitListPage.jsx
+++ src/components/waitlist/WaitListPage.jsx
@@ -3,19 +3,33 @@ import WaitListForm from "./WaitListForm";
 
 export default function WaitListPage() {
     return (
-        <>
-            <div className="relative">
-                {/* Background Image with Blur and Opacity */}
-                <div
-                    className="absolute inset-0 bg-cover bg-center filter blur-sm opacity-50"
-                    style={{ backgroundImage: 'url(/images/bg.png)' }}
-                ></div>
+        <div className="relative w-full min-h-[100vh]">
+            {/* Background Image */}
+            <div
+                className="absolute inset-0 bg-cover bg-center bg-[url('/images/bg.png')]"
+            ></div>
 
-                {/* Black Overlay with Glass Effect */}
-                <div className="relative px-5 md:px-10 py-10 lg:px-16 w-full min-h-[100vh] flex justify-center items-center bg-transparent">
-                  
+            {/* Black Overlay with Glass Effect */}
+            <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-md flex justify-center items-center">
+                <div className="relative z-10 px-5 md:px-10 py-10 lg:px-16 flex flex-col items-center text-white">
+                    {/* app name */}
+                    <div className="mb-5">
+                        <h2 className="uppercase text-center tracking-widest">
+                            {config.appName}.so
+                        </h2>
+                    </div>
+
+                    <h1 className="text-center">
+                        To-dos and Schedules in one place.
+                    </h1>
+
+                    {/* form */}
+                    <div className="mt-5">
+                        <h3 className="text-center">Never miss a task again!</h3>
+                        <WaitListForm />
+                    </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
