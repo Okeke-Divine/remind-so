3b881a1e494afe330bf74a8099864d5944f2c067
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-08-10 17:11:52 +0100

    Updated the sign in page

--- src/components/shared/AuthPageAside.jsx
+++ src/components/shared/AuthPageAside.jsx
@@ -1,7 +1,9 @@
+import { auth_page_aside_text } from "@/constants/text";
+
 export default function AuthPageAside({ w }) {
     return (
         <div className={`${w} w-1/2 font-bold bg-black text-white grid items-center text-4xl text-center uppercase min-h-[100vh]`}>
-            Join thousands of indie hackers who've ...
+            {auth_page_aside_text}
         </div>
     )
 }
--- src/constants/text.js
+++ src/constants/text.js
@@ -1,2 +1,3 @@
 export const error_msg = "An error occured please try again.";
 export const joined_waitlist = "Thank you for joining the waitlist! We're excited to have you on board and will keep you updated with the latest news. Stay tuned!";
+export const auth_page_aside_text = "";
