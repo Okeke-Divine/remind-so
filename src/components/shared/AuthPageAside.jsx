ff7009d5dcfe89d20d0dbf264829f6db510f9475
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-08-10 16:59:36 +0100

    Updated the sign in page

--- src/components/shared/AuthPageAside.jsx
+++ src/components/shared/AuthPageAside.jsx
@@ -1,6 +1,6 @@
 export default function AuthPageAside({ w }) {
     return (
-        <div className={`${w} w-1/2 font-bold bg-black text-white grid items-center text-4xl text-center uppercase`}>
+        <div className={`${w} w-1/2 font-bold bg-black text-white grid items-center text-4xl text-center uppercase min-h-[100vh]`}>
             Join thousands of indie hackers who've ...
         </div>
     )
