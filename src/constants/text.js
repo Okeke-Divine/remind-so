24631a08cfeeab99c3d8db6a345ae5d9c7b5e1b1
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-10-06 12:55:32 +0100

    Updated the auth page aside

--- src/constants/text.js
+++ src/constants/text.js
@@ -1,3 +1,3 @@
 export const error_msg = "An error occured please try again.";
 export const joined_waitlist = "Thank you for joining the waitlist! We're excited to have you on board and will keep you updated with the latest news. Stay tuned!";
-export const auth_page_aside_text = "Join thousands of indie hackers who've optimized their workflow with custom reminders. Get started today!"
+export const auth_page_aside_text = "Join thousands of indie hackers who've optimized their workflow with custom reminders."
